148cc78049637491f8ad05930a4630bf2b6504d4
Juan <juan.giana8589@gmail.com>
2023-05-31 20:19:23 -0500

    add todo route all-to-dos, new-todo, todoById, todoByTitle,

--- routes/todos.js
+++ routes/todos.js
@@ -1,89 +1,74 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../model/Todo");
+const Todo = require("../model/Todo");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
-// router.get("/all-users", async (req, res) => {
-//   try {
-//     const allUsers = await User.find({});
-//     res.status(200).json({ success: true, data: allUsers });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ success: false, massage: error.massage });
-//   }
-// });
+router.get("/all-to-dos", async (req, res) => {
+  // http://localhost:3000/todos/all-to-dos
+  try {
+    const allToDos = await Todo.find({});
+    res.status(200).json({ success: true, data: allToDos });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, massage: error.massage });
+  }
+});
 
-// router.post("/new-user", async (req, res) => {
-//   try {
-//     const { name, email, password, phone } = req.body;
-//     const user = {
-//       name: name,
-//       email: email,
-//       password: password,
-//       phone: phone,
-//     };
-//     const newUser = await new User(user); // calls user model
-//     const saveUser = await newUser.save();
-//     res.status(200).json({ success: true, data: saveUser });
-//   } catch (error) {
-//     res.status(500).json({ success: false, massage: error.message });
-//   }
-// });
+router.post("/new-todo", async (req, res) => {
+  // http://localhost:3000/todos/new-todo
+  try {
 
-// router.get("/userByEmail/:email", async (req, res) => {
-//   try {
-//     const targetEmail = req.params.email;
-//     const targetUser = await User.findOne({ email: targetEmail });
-//     if (!targetUser) {
-//       return res
-//         .status(400)
-//         .json({ success: false, message: "User not found" });
-//     }
-//     res.status(200).json({ success: true, data: targetUser });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ success: false, massage: error.massage });
-//   }
-// });
+    const { title, description } = req.body;
+    const todo = {
+      title: title,
+      description: description,
+    };
 
-// router.get("/userByName/:name", async (req, res) => {
-//   // http://localhost:3000/users/userByName/giana
-//   try {
-//     const targetName = req.params.name;
-//     const targetUser = await User.find({ name: targetName });
-//     if (!targetName) {
-//       return res
-//         .status(400)
-//         .json({ success: false, message: "User not found" });
-//     }
-//     res.status(200).json({ success: true, data: targetUser });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ success: false, massage: error.massage });
-//   }
-// });
+    const newTodo = await new Todo(todo); // calls user model
+    const saveTodo = await newTodo.save();
+    res.status(200).json({ success: true, data: saveTodo });
+  } catch (error) {
+    res.status(500).json({ success: false, massage: error.message });
+  }
+});
 
-// router.get("/userByName", async (req, res) => {
-//   // http://localhost:3000/users/userByName?name=giana
-//   try {
-//     const targetName = req.query.name;
-//     const targetUser = await User.find({ name: targetName }); // or ({ name: { $regex: new RegExp(name, "i") }});
+router.get("/todoById/:id", async (req, res) => {
+  // http://localhost:3000/todos/todoByID/ad35358f-8fe6-4555-aa6a-fc8014a283df
+  try {
+    const targetID = req.params.id;
+    const targetTodo = await Todo.findById({ _id: targetID });
+    if (!targetTodo) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+    res.status(200).json({ success: true, data: targetTodo });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, massage: error.massage });
+  }
+});
 
-//     if (!targetUser) {
-//       return res
-//         .status(400)
-//         .json({ success: false, message: "User not found" });
-//     }
-//     res.status(200).json({ success: true, data: targetUser });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ success: false, message: error.message });
-//   }
-// });
+router.get("/todoByTitle/:title", async (req, res) => {
+  // http://localhost:3000/todos/todoByTitle/pick up the milk
+  try {
+    const targetTitle = req.params.title;
+    const targetTodo = await Todo.find({ title: targetTitle });
+    if (!targetTodo) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+    res.status(200).json({ success: true, data: targetTodo });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, massage: error.massage });
+  }
+});
 
 // router.put("/update-user/:id", async (req, res) => {
 //   // http://localhost:3000/users/update-user/853e0770-8c89-47a5-b890-504cfef67c3c
